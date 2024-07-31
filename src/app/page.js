6ebcf95a38126d05056f8a11b474b0903cb8249d
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2024-07-31 00:42:25 -0400

    firebase link

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,27 @@
-import { Box, Stack, Typography } from "@mui/material";
+"use client";
 
-const items = [
-  "tomate",
-  "potato",
-  "onion",
-  "garlic",
-  "carrot",
-  "lettuce",
-  "ginger",
-  "kale",
-];
+import { Box, Stack, Typography } from "@mui/material";
+import { Firestore, getDocs, query } from "firebase/firestore";
+import { collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { firestore } from "../../firebase";
 
 export default function Home() {
+  const [pantry, setPantry] = useState([]);
+
+  useEffect(() => {
+    const updatePantry = async () => {
+      const snapshot = query(collection(firestore, "pantry"));
+      const docs = getDocs(snapshot);
+      const pantryList = [];
+      (await docs).forEach((doc) => {
+        pantryList.push(doc.id);
+        setPantry(pantryList);
+      });
+      console.log(pantryList);
+    };
+    updatePantry();
+  }, []);
   return (
     <Box className="flex flex-col items-center justify-center relative h-screen">
       <Box className="border border-solid border-black ">
@@ -21,9 +31,10 @@ export default function Home() {
           </Typography>
         </Box>
         <Stack width="800px" height="200px" spacing={2} overflow={"auto"}>
-          {items.map((i) => (
+          {pantry.map((i) => (
             <Box
               key={i}
+              minHeight={"100px"}
               className="flex items-center justify-center relative h-screen bg-[#f0f0f0]"
             >
               <Typography variant={"h5"} className="text-[#333] text-center">
