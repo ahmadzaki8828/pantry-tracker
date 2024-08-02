ffde1a56042689b198c37fac8adabfd530997bc0
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2024-08-01 23:29:15 -0400

    typo fix

--- src/app/page.js
+++ src/app/page.js
@@ -164,7 +164,7 @@ export default function Home() {
               <SearchIcon />
             </SearchIconWrapper>
             <StyledInputBase
-              placeholder="Search…"
+              placeholder="Search"
               inputProps={{ "aria-label": "search" }}
               value={searchQuery}
               onChange={handleSearchChange}
